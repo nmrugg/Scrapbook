37017e7bc83b106460adab4223762f9e904a6d72
nmrugg <nmrugg@gmail.com>
2010-12-06 01:31:57 -0600

    Rotated but does not follow because the rot_x, rot_y values are not yet updated when the layer moves.

--- scrapbook.js
+++ scrapbook.js
@@ -41,71 +41,55 @@
         
         function draw_decoration(cur_layer)
         {
-            var x1,
-                x2,
-                x3,
-                x4,
-                y1,
-                y2,
-                y3,
-                y4;
+            var cur_x = cur_layer.x,
+                cur_y = cur_layer.y,
+                cur_x2,
+                cur_y2;
             
-            if (cur_layer.angle === 0) {
-                x1 = cur_layer.x;
-                y1 = cur_layer.y;
-                
-                x2 = x1 + cur_layer.width;
-                y2 = y1 + cur_layer.height;
-                
-                x3 = x1;
-                y3 = y2;
-                
-                x4 = x2;
-                y4 = y1;
-            } else {
-                x1 = cur_layer.rot_x1;
-                x2 = cur_layer.rot_x2;
-                x3 = cur_layer.rot_x3;
-                x4 = cur_layer.rot_x4;
-                
-                y1 = cur_layer.rot_y1;
-                y2 = cur_layer.rot_y2;
-                y3 = cur_layer.rot_y3;
-                y4 = cur_layer.rot_y4;
-            }
+            cur_x2 = cur_x + cur_layer.width;
+            cur_y2 = cur_y + cur_layer.height;
             
             context.save();
+            
+            if (cur_layer.angle != 0) {
+                cur_x = 0;
+                cur_y = 0;
+                cur_x2 = cur_layer.width;
+                cur_y2 = cur_layer.height;
+                context.translate(cur_layer.rot_x1, cur_layer.rot_y1);
+                context.rotate(cur_layer.angle * Math.PI / 180);
+            }
             
             switch (cur_decoration_mode) {
             case decoration_resize:
                 /// Upper left square
-                context.moveTo(x1 - 4.5, y1 - 4.5);
-                context.lineTo(x1 + 6.5, y1 - 4.5);
-                context.lineTo(x1 + 6.5, y1 + 6.5);
-                context.lineTo(x1 - 4.5, y1 + 6.5);
-                context.lineTo(x1 - 4.5, y1 - 4.5);
+                context.moveTo(cur_x - 4.5, cur_y - 4.5);
+                context.lineTo(cur_x + 6.5, cur_y - 4.5);
+                context.lineTo(cur_x + 6.5, cur_y + 6.5);
+                context.lineTo(cur_x - 4.5, cur_y + 6.5);
+                context.lineTo(cur_x - 4.5, cur_y - 4.5);
 
                 
                 /// Upper right square
-                context.moveTo(x2 - 6.5, y2 - 4.5);
-                context.lineTo(x2 + 4.5, y2 - 4.5);
-                context.lineTo(x2 + 4.5, y2 + 6.5);
-                context.lineTo(x2 - 6.5, y2 + 6.5);
-                context.lineTo(x2 - 6.5, y2 - 4.5);
+                context.moveTo(cur_x2 - 6.5, cur_y - 4.5);
+                context.lineTo(cur_x2 + 4.5, cur_y - 4.5);
+                context.lineTo(cur_x2 + 4.5, cur_y + 6.5);
+                context.lineTo(cur_x2 - 6.5, cur_y + 6.5);
+                context.lineTo(cur_x2 - 6.5, cur_y - 4.5);
                 
                 /// lower left square
-                context.moveTo(x3 - 4.5, y3 - 6.5);
-                context.lineTo(x3 + 6.5, y3 - 6.5);
-                context.lineTo(x3 + 6.5, y3 + 4.5);
-                context.lineTo(x3 - 4.5, y3 + 4.5);
-                context.lineTo(x3 - 4.5, y3 - 6.5);
+                context.moveTo(cur_x - 4.5, cur_y2 - 6.5);
+                context.lineTo(cur_x + 6.5, cur_y2 - 6.5);
+                context.lineTo(cur_x + 6.5, cur_y2 + 4.5);
+                context.lineTo(cur_x - 4.5, cur_y2 + 4.5);
+                context.lineTo(cur_x - 4.5, cur_y2 - 6.5);
                 
                 /// lower right square
-                context.moveTo(x4 - 6.5, y4 - 6.5);
-                context.lineTo(x4 + 4.5, y4 - 6.5);
-                context.lineTo(x4 + 4.5, y4 + 4.5);
-                context.lineTo(x4 - 6.5, y4 + 4.5);
-                context.lineTo(x4 - 6.5, y4 - 6.5);
+                context.moveTo(cur_x2 - 6.5, cur_y2 - 6.5);
+                context.lineTo(cur_x2 + 4.5, cur_y2 - 6.5);
+                context.lineTo(cur_x2 + 4.5, cur_y2 + 4.5);
+                context.lineTo(cur_x2 - 6.5, cur_y2 + 4.5);
+                context.lineTo(cur_x2 - 6.5, cur_y2 - 6.5);
                 context.strokeStyle = "rgba(0,0,0,.5)";
                 context.stroke();
                 
@@ -354,6 +338,7 @@
                             cur_layer = layers[selected_layer];
                             cur_layer.x = layer_starting_x + (cur_x - mouse_starting_x);
                             cur_layer.y = layer_starting_y + (cur_y - mouse_starting_y);
+                            
                             /// Figure out a way to tell the canvas to only redraw the part that changed.
                             redraw();
                         }
